918a56ee73de6c7b2185432d226b620df89d27be
Marvin Scharle <marvin.scharle@conclurer.com>
2020-02-07 21:30:12 +0100

    [add] jest

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+process.env.TESTING = 'true';
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^testing-tools/(.*)$': '<rootDir>/testing-tools/$1',
+  },
+  snapshotSerializers: [],
+  testMatch: ['**/*.spec.(js|jsx|ts|tsx)'],
+  testURL: 'http://localhost/',
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    '(^src)',
+    'testing-tools',
+    'migrations',
+    'node_modules',
+  ],
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 99,
+  //     functions: 99,
+  //     lines: 99,
+  //     statements: 99,
+  //   },
+  // },
+};
---
+++ run-tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+export DB_PORT=5433
+export DB_NAME=test-opdb
+export POSTGRES_USER=test-opdb
+export POSTGRES_PASSWORD=test-opdb
+
+(docker kill $DB_NAME || true) > /dev/null 2> /dev/null
+docker run \
+  --name $DB_NAME \
+  --detach --rm \
+  -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+  -e POSTGRES_USER=$POSTGRES_USER \
+  -p $DB_PORT:5432 \
+  postgres \
+  > /dev/null
+
+echo Using connection URL: $DATABASE_URL
+
+sleep 5
+
+# postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...]
+export DATABASE_URL="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@127.0.0.1:$DB_PORT/${DB_NAME}"
+DATABASE_LOGGING="error,warn" yarn jest --config jest.config.js --runInBand $@
+
+docker kill $DB_NAME > /dev/null
