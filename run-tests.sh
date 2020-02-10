9a56ba6339725d0ad0570dabcb142e5716b0a1b2
Marvin Scharle <marvin.scharle@conclurer.com>
2020-02-10 23:53:48 +0100

    [add] mysql docker image

--- run-tests.sh
+++ run-tests.sh
@@ -2,8 +2,9 @@
 
 set -e
 
+# Start PostgreSQL
 export TEST_PG_DB_PORT=5433
-export TEST_PG_DB_NAME=test-opdb
+export TEST_PG_DB_NAME=pg-test-opdb
 export TEST_PG_USER=test-opdb
 export TEST_PG_PASSWORD=test-opdb
 export TEST_PG_DB_URL="postgres://$TEST_PG_USER:$TEST_PG_PASSWORD@127.0.0.1:$TEST_PG_DB_PORT/$TEST_PG_DB_NAME"
@@ -18,9 +19,29 @@ docker run \
   postgres \
   > /dev/null
 
+# Start MySQL
+export TEST_MYSQL_DB_PORT=3307
+export TEST_MYSQL_DB_NAME=mysql-test-opdb
+export TEST_MYSQL_USER=test-opdb
+export TEST_MYSQL_PASSWORD=test-opdb
+export TEST_MYSQL_DB_URL="mysql://$TEST_MYSQL_USER:$TEST_MYSQL_PASSWORD@127.0.0.1:$TEST_MYSQL_DB_PORT/$TEST_MYSQL_DB_NAME"
+
+(docker kill $TEST_MYSQL_DB_NAME || true) > /dev/null 2> /dev/null
+docker run \
+  --name $TEST_MYSQL_DB_NAME \
+  --detach --rm \
+  -e MYSQL_DATABASE=$TEST_MYSQL_DB_NAME \
+  -e MYSQL_USER=$TEST_MYSQL_USER \
+  -e TEST_MYSQL_PASSWORD=$TEST_MYSQL_PASSWORD \
+  -p $TEST_MYSQL_DB_PORT:3306 \
+  mysql \
+  > /dev/null
+
 sleep 5
 
 # shellcheck disable=SC2068
 yarn jest --config jest.config.js --runInBand $@
 
+# Kill docker container
 docker kill $TEST_PG_DB_NAME > /dev/null
+docker kill $TEST_MYSQL_DB_NAME > /dev/null
