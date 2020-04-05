0fdca407222811181a3900e969e4dcb77c446ed6
Marvin Scharle <marvin.scharle@conclurer.com>
2020-04-05 14:32:53 +0200

    [update] build script

--- build.sh
+++ build.sh
@@ -18,7 +18,8 @@ cd "$baseDir"
 yarn cpx "./src/base/dist/**/*.{ts,js}" "./dist/opdb-base"
 yarn cpx "./src/base/package.json" "./dist/opdb-base"
 yarn cpx "./src/base/README.md" "./dist/opdb-base"
-node ./adjust-package-file.js ./dist/opdb-base/package.json
+yarn cpx "./src/base/CHANGELOG.md" "./dist/opdb-base"
+yarn cpx "./src/base/CHANGELOG.json" "./dist/opdb-base"
 rm -rf ./src/base/dist
 
 # @opdb/postgres
@@ -29,7 +30,8 @@ cd "$baseDir"
 yarn cpx "./src/adapter/postgres/dist/**/*.{ts,js}" "./dist/opdb-postgres"
 yarn cpx "./src/adapter/postgres/package.json" "./dist/opdb-postgres"
 yarn cpx "./src/adapter/postgres/README.md" "./dist/opdb-postgres"
-node ./adjust-package-file.js ./dist/opdb-postgres/package.json
+yarn cpx "./src/adapter/postgres/CHANGELOG.md" "./dist/opdb-postgres"
+yarn cpx "./src/adapter/postgres/CHANGELOG.json" "./dist/opdb-postgres"
 rm -rf ./src/adapter/postgres/dist
 
 # @opdb/mysql
@@ -40,5 +42,6 @@ cd "$baseDir"
 yarn cpx "./src/adapter/mysql/dist/**/*.{ts,js}" "./dist/opdb-mysql"
 yarn cpx "./src/adapter/mysql/package.json" "./dist/opdb-mysql"
 yarn cpx "./src/adapter/mysql/README.md" "./dist/opdb-mysql"
-node ./adjust-package-file.js ./dist/opdb-mysql/package.json
+yarn cpx "./src/adapter/mysql/CHANGELOG.md" "./dist/opdb-mysql"
+yarn cpx "./src/adapter/mysql/CHANGELOG.json" "./dist/opdb-mysql"
 rm -rf ./src/adapter/mysql/dist
