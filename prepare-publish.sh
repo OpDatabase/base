29dc1f0cb824c351dfd6c191eba17d9c2c624c45
Marvin Scharle <marvin.scharle@conclurer.com>
2020-04-05 16:09:40 +0200

    Update env vars for github credentials

--- prepare-publish.sh
+++ prepare-publish.sh
@@ -2,4 +2,4 @@
 
 git config user.email "marvin.scharle@conclurer.com"
 git config user.name "Marvin Scharle"
-git remote set-url origin "https://${GIT_USERNAME}:${GIT_ACCESS_TOKEN}@github.com/OpDatabase/base.git"
+git remote set-url origin "https://$GIT_USERNAME:$GIT_ACCESS_TOKEN@github.com/OpDatabase/base.git"
