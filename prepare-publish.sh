3def505d4feb215422805ef188c29f62ad554f19
Marvin Scharle <marvin.scharle@conclurer.com>
2020-04-05 14:46:11 +0200

    [update] build script

---
+++ prepare-publish.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git config user.email "marvin.scharle@conclurer.com"
+git config user.name "Marvin Scharle"
+git remote set-url origin "https://${GIT_USERNAME}:${GIT_ACCESS_TOKEN}@github.com/OpDatabase/base.git"
