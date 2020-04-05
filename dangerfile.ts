ea5282937087298d5638f9a096b75c8228be60eb
Marvin Scharle <marvin.scharle@conclurer.com>
2020-04-05 18:57:06 +0200

    update build scripts

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,8 +1,1 @@
-// tslint:disable-next-line:no-implicit-dependencies
-import { fail } from 'danger';
-import { existsSync, readdirSync } from 'fs';
 
-// Check if all beachball changes have been applied
-if (existsSync('./change') && readdirSync('./change').length > 0) {
-  fail(`There are pending beachball changes. To apply them, run: "beachball bump"!`);
-}
