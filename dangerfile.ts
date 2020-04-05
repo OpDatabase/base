292f18d590f5080c62eed79b456e935dfc6e4478
Marvin Scharle <marvin.scharle@conclurer.com>
2020-04-05 18:41:13 +0200

    yarn -> npm

--- dangerfile.ts
+++ dangerfile.ts
@@ -4,5 +4,5 @@ import { existsSync, readdirSync } from 'fs';
 
 // Check if all beachball changes have been applied
 if (existsSync('./change') && readdirSync('./change').length > 0) {
-  fail(`There are pending beachball changes. To apply them, run: "yarn beachball bump"!`);
+  fail(`There are pending beachball changes. To apply them, run: "beachball bump"!`);
 }
