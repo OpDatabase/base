6be48bda4623b406c0b1149166caec93921d5de5
Marvin Scharle <marvin.scharle@conclurer.com>
2020-04-05 17:31:52 +0200

    [add] danger

--- dangerfile.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const danger = require('danger');
-
-// Check if all beachball changes have been applied
-if (fs.existsSync('./change') && fs.readdirSync('./change').length > 0) {
-  danger.fail(`There are pending beachball changes. To apply them, run: "yarn beachball bump"!`);
-}
-
---
+++ dangerfile.ts
@@ -0,0 +1,7 @@
+import { fail } from 'danger';
+import { existsSync, readdirSync } from 'fs';
+
+// Check if all beachball changes have been applied
+if (existsSync('./change') && readdirSync('./change').length > 0) {
+  fail(`There are pending beachball changes. To apply them, run: "yarn beachball bump"!`);
+}
