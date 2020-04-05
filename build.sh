003303b03c59089e980153224b70c683cd92bdd5
Marvin Scharle <marvin.scharle@conclurer.com>
2020-04-05 22:06:33 +0200

    [update] build script for orm

--- build.sh
+++ build.sh
@@ -27,8 +27,9 @@ rm -rf ./src/base/dist
 
 # @opdb/postgres
 cd ./src/adapter/postgres
-npm link ../../../dist/opdb-base
+npm link ../../base
 npm i
+npm link ../../base
 npm run build
 cd "$baseDir"
 cpx "./src/adapter/postgres/dist/**/*.{ts,js}" "./dist/opdb-postgres"
@@ -40,8 +41,9 @@ rm -rf ./src/adapter/postgres/dist
 
 # @opdb/mysql
 cd ./src/adapter/mysql
-npm link ../../../dist/opdb-base
+npm link ../../base
 npm i
+npm link ../../base
 npm run build
 cd "$baseDir"
 cpx "./src/adapter/mysql/dist/**/*.{ts,js}" "./dist/opdb-mysql"
@@ -53,12 +55,27 @@ rm -rf ./src/adapter/mysql/dist
 
 # @opdb/orm
 cd ./src/orm
-yarn
-yarn build
+npm link ../base
+npm link ../adapter/postgres
+npm link ../adapter/mysql
+# cpx "../../dist/opdb-base/**/*" "node_modules/@opdb/base"
+# cpx "../../dist/opdb-postgres/**/*" "node_modules/@opdb/postgres"
+# cpx "../../dist/opdb-mysql/**/*" "node_modules/@opdb/mysql"
+npm i
+npm link ../base
+npm link ../adapter/postgres
+npm link ../adapter/mysql
+npm run build
 cd "$baseDir"
-yarn cpx "./src/orm/dist/**/*.{ts,js}" "./dist/opdb-orm"
-yarn cpx "./src/orm/**/*._ts" "./dist/opdb-orm"
-yarn cpx "./src/orm/package.json" "./dist/opdb-orm"
-yarn cpx "./src/orm/README.md" "./dist/opdb-orm"
-node ./adjust-package-file.js ./dist/opdb-orm/package.json
+cpx "./src/orm/dist/**/*.{ts,js}" "./dist/opdb-orm"
+cpx "./src/orm/**/*._ts" "./dist/opdb-orm"
+cpx "./src/orm/package.json" "./dist/opdb-orm"
+cpx "./src/orm/README.md" "./dist/opdb-orm"
 rm -rf ./src/orm/dist
+
+
+## Remove node_modules within dist
+# rm -rf ./dist/opdb-base/node_modules
+# rm -rf ./dist/opdb-postgres/node_modules
+# rm -rf ./dist/opdb-mysql/node_modules
+# rm -rf ./dist/opdb-orm/node_modules
